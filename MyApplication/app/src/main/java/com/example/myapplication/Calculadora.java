69c9e7f1c86510f1318b6535680df0724c59cc9a
Deckconer <briankira100@gmail.com>
2022-10-02 19:25:15 +0200

    l

--- MyApplication/app/src/main/java/com/example/myapplication/Calculadora.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Calculadora.java
@@ -3,12 +3,31 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Calculadora extends AppCompatActivity {
+
+    private Button btnSuma;
+    private Button btnRestar;
+    private Button btnMultiplicar;
+    private Button btnDividir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
+
+        btnSuma = findViewById(R.id.btnSuma);
+        btnRestar = findViewById(R.id.btnRestar);
+        btnMultiplicar = findViewById(R.id.btnMultiplicar);
+        btnDividir = findViewById(R.id.btnDividir);
+
+        btnSuma.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCalculadora;
+    private Button btnComprobarDNI;
+    private Button btnFPantalla;
+    private Button btnDomotica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCalculadora = findViewById(R.id.btnCalculadora);
+        btnComprobarDNI = findViewById(R.id.btnComprobarDNI);
+        btnFPantalla = findViewById(R.id.btnFPantalla);
+        btnDomotica = findViewById(R.id.btnDomotica);
+
+        btnCalculadora.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Calculadora.class);
+                startActivity(intent);
+            }
+        });
     }
 }
