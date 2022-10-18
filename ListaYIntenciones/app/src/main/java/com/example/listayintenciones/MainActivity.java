8a0a133500efb1b11ba560f127e78bb71a4269b1
Deckoner <briankira100@gmail.com>
2022-10-18 13:30:59 +0200

    SDFSDF

--- ListaYIntenciones/app/src/main/java/com/example/listayintenciones/MainActivity.java
+++ ListaYIntenciones/app/src/main/java/com/example/listayintenciones/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.listayintenciones;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner sPaises;
+    private TextView tvResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sPaises = findViewById(R.id.sPaises);
+
+        tvResultado = findViewById(R.id.tvResultado);
+
+
+        tvResultado.setText();
+
+        sPaises.getSelectedItem();
     }
 }
